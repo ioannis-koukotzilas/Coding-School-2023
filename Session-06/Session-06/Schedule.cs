74df0ce5dcba8ec30332ba42c6243492e9753206
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-16 17:35:33 +0200

    Add schedule

---
+++ Session-06/Session-06/Schedule.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06 {
+
+    public class Schedule {
+
+        public Guid ID { get; set; }
+        public Guid CourseID { get; set; }
+        public Guid ProfessorID { get; set; }
+        public DateTime Calendar { get; set; }
+
+        public Schedule() { }
+
+        public Schedule(Guid id) : this() {
+            ID = id;
+        }
+
+        public Schedule(Guid id, Guid courseId) : this(id) {
+            CourseID = courseId;
+        }
+
+        public Schedule(Guid id, Guid courseId, Guid professorId) : this(id, courseId) {
+            ProfessorID = professorId;
+        }
+
+        public Schedule(Guid id, Guid courseId, Guid professorId, DateTime calendar) : this(id, courseId, professorId) {
+            Calendar = calendar;
+        }
+
+    }
+
+}
