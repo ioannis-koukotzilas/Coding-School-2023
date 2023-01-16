0cda9ebdbb4d36cb45be21a43a497d5b99240971
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-16 15:23:59 +0200

    Add person

---
+++ Session-06/Session-06/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Session_06 {
+    public class Person {
+
+        // Properties
+
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        // Constructors
+
+        public Person() { }
+
+        public Person(Guid id) : this() {
+            Id = id;
+        }
+
+        public Person(Guid id, string name ) : this(id) {
+            Name = name;
+        }
+
+        public Person(Guid id, string name, int age ) : this(id, name) {
+            Age = age;
+        }
+
+        // Methods
+
+        public void GetName() {}
+
+        public void SetName(string Name) {
+            Console.WriteLine($"Name is: {Name}.");
+        }
+
+    }
+}
--- Session-06/Session-06/Program.cs
+++ Session-06/Session-06/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program {
+﻿using Session_06;
+using System.Xml.Linq;
+
+internal class Program {
     private static void Main(string[] args) {
 
-        Console.WriteLine("Hello, World!");
+        Person person = new Person();
+
+        person.SetName("Ioannis Koukotzilas");
 
         Console.ReadLine();
 
