ddd88689c1f5778b38630541c50959b569ab1792
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-18 15:11:43 +0200

    Rewrite the proram from scratch

--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -5,59 +5,93 @@ using Session_07;
 internal class Program {
     private static void Main(string[] args) {
 
-        ActionResolver resolver = new ActionResolver();
+        // REWRITE THE PROGRAM!
 
-        { // UPPERCASE
-
-            ActionRequest request = new ActionRequest();
-            request.Action = ActionEnum.Uppercase;
-            request.Input = "here is a multiword input";
-
-
-            ActionResponse response = resolver.Execute(request);
-            Console.WriteLine(response.Output);
-        }
-
-        { // REVERSE
-
-            ActionRequest request = new ActionRequest();
-            request.Action = ActionEnum.Reverse;
-            request.Input = "sdrawkcab";
-
-            ActionResponse response = resolver.Execute(request);
-            Console.WriteLine(response.Output);
-        }
-
-
-        { // CONVERT
-
-            ActionRequest request = new ActionRequest();
-            request.Action = ActionEnum.Convert;
-            request.Input = "123";
-
-            ActionResponse response = resolver.Execute(request);
-            Console.WriteLine(response.Output);
-        }
-
-        { // ERROR
-
-            ActionRequest request = new ActionRequest();
-            request.Action = ActionEnum.Convert;
-            request.Input = "asdf234"; // Error message should be generated
-
-            ActionResponse response = resolver.Execute(request);
-            Console.WriteLine(response.Output);
-        }
-
-        Message msg = new Message();
-        msg.Data = "Message 1";
-        msg.printMessage();
-
-        Message msg2 = new ErrorMessage();
-        msg2.Data = "Message 2";
-        msg2.printMessage();
+        Console.WriteLine("REWRITE THE PROGRAM!");
 
         Console.ReadLine();
+
     }
 
 }
+
+
+
+public class ActionRequest {
+
+}
+
+public class ActionResponse {
+
+}
+
+public class ActionResolver {
+
+}
+
+public class MessageLogger {
+
+}
+
+public class Message {
+
+}
+
+
+
+
+
+
+
+
+//ActionResolver resolver = new ActionResolver();
+
+//{ // UPPERCASE
+
+//    ActionRequest request = new ActionRequest();
+//    request.Action = ActionEnum.Uppercase;
+//    request.Input = "here is a multiword input";
+
+
+//    ActionResponse response = resolver.Execute(request);
+//    Console.WriteLine(response.Output);
+//}
+
+//{ // REVERSE
+
+//    ActionRequest request = new ActionRequest();
+//    request.Action = ActionEnum.Reverse;
+//    request.Input = "sdrawkcab";
+
+//    ActionResponse response = resolver.Execute(request);
+//    Console.WriteLine(response.Output);
+//}
+
+
+//{ // CONVERT
+
+//    ActionRequest request = new ActionRequest();
+//    request.Action = ActionEnum.Convert;
+//    request.Input = "123";
+
+//    ActionResponse response = resolver.Execute(request);
+//    Console.WriteLine(response.Output);
+//}
+
+//{ // ERROR
+
+//    ActionRequest request = new ActionRequest();
+//    request.Action = ActionEnum.Convert;
+//    request.Input = "asdf234"; // Error message should be generated
+
+//    ActionResponse response = resolver.Execute(request);
+//    Console.WriteLine(response.Output);
+//}
+
+//Message msg = new Message();
+//msg.Data = "Message 1";
+//msg.printMessage();
+
+//Message msg2 = new ErrorMessage();
+//msg2.Data = "Message 2";
+//msg2.printMessage();
