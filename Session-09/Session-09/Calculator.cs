fa6c0a8c72c398d6f16312ae109a0c0d9d84fee0
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-19 18:10:40 +0200

    Add clear monitor method

--- Session-09/Session-09/Calculator.cs
+++ Session-09/Session-09/Calculator.cs
@@ -32,36 +32,53 @@ namespace Session_09 {
             }
         }
 
+
+        // TODO: Clear monitor needs improvements
+        public void ClearMonitor() {
+            if (_result != null) {
+                Monitor.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+        }
+
         // OPERATIONS EVENTS
 
         private void BtnAdditionClick(object sender, EventArgs e) {
             Monitor.Text += " + ";
             _calculatorOperation = CalcuratorOperation.Addition;
+            ClearMonitor();
         }
 
         private void BtnSubtractionClick(object sender, EventArgs e) {
             Monitor.Text += " - ";
             _calculatorOperation = CalcuratorOperation.Subtraction;
+            ClearMonitor();
         }
 
         private void BtnMultiplicationClick(object sender, EventArgs e) {
             Monitor.Text += " x ";
             _calculatorOperation = CalcuratorOperation.Multiplication;
+            ClearMonitor();
         }
 
         private void BtnDivisionClick(object sender, EventArgs e) {
             Monitor.Text += " ÷ ";
             _calculatorOperation = CalcuratorOperation.Division;
+            ClearMonitor();
         }
 
         private void BtnRaiseToPowerClick(object sender, EventArgs e) {
             Monitor.Text += " n2 ";
             _calculatorOperation = CalcuratorOperation.RaiseToPower;
+            ClearMonitor();
         }
 
         private void BtnSquareRootClick(object sender, EventArgs e) {
             Monitor.Text += " √ ";
             _calculatorOperation = CalcuratorOperation.SquareRoot;
+            ClearMonitor();
         }
 
         private void BtnEqualsClick(object sender, EventArgs e) {
@@ -104,6 +121,7 @@ namespace Session_09 {
             }
 
             Monitor.Text += _result;
+  
         }
 
         // NUMBERS EVENTS
@@ -111,46 +129,55 @@ namespace Session_09 {
         private void BtnOneClick(object sender, EventArgs e) {
             Monitor.Text += " 1 ";
             AssignValue(1);
+            ClearMonitor();
         }
 
         private void BtnTwoClick(object sender, EventArgs e) {
             Monitor.Text += " 2 ";
             AssignValue(2);
+            ClearMonitor();
         }
 
         private void BtnThreeClick(object sender, EventArgs e) {
             Monitor.Text += " 3 ";
             AssignValue(3);
+            ClearMonitor();
         }
 
         private void BtnFourClick(object sender, EventArgs e) {
             Monitor.Text += " 4 ";
             AssignValue(4);
+            ClearMonitor();
         }
 
         private void BtnFiveClick(object sender, EventArgs e) {
             Monitor.Text += " 5 ";
             AssignValue(5);
+            ClearMonitor();
         }
 
         private void BtnSixClick(object sender, EventArgs e) {
             Monitor.Text += " 6 ";
             AssignValue(6);
+            ClearMonitor();
         }
 
         private void BtnSevenClick(object sender, EventArgs e) {
             Monitor.Text += " 7 ";
             AssignValue(7);
+            ClearMonitor();
         }
 
         private void BtnEightClick(object sender, EventArgs e) {
             Monitor.Text += " 8 ";
             AssignValue(8);
+            ClearMonitor();
         }
 
         private void BtnNineClick(object sender, EventArgs e) {
             Monitor.Text += " 9 ";
             AssignValue(9);
+            ClearMonitor();
         }
 
     }
