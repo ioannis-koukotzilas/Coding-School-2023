752f2d9005c83591147683e92346ee073e320834
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-30 20:54:01 +0200

    Add customer, product and product category models

---
+++ Session-16/CoffeeShop.Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace CoffeeShop.Models {
+    public class Product {
+
+        public Guid ID { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public decimal Cost { get; set; }
+        public Guid ProductCategoryID { get; set; }
+
+        public Product(string code, string description) {
+            ID = Guid.NewGuid();
+            Code = code;
+            Description = description;
+        }
+
+    }
+}
