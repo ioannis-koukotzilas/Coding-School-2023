7c5da056435d27aaa3f30eac05f661b217b39936
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-30 21:03:19 +0200

    Add CoffeeShop ORM, install EF packages, create products DbSet

---
+++ Session-16/CoffeeShop.ORM/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CoffeeShop.ORM {
+
+    public class AppDbContext : DbContext {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoffeeshopDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+
+}
