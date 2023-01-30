52576c4a75efa3e2c8dbde813022b9a4e8c4cb53
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-30 23:56:42 +0200

    Add transaction line model

---
+++ Session-16/CoffeeShop.Models/TransactionLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Models {
+    public class TransactionLine {
+
+        public Guid ID { get; set; }
+        public Guid ProductID { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public int Discount { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public TransactionLine() {
+            ID = Guid.NewGuid();
+        }
+
+    }
+}
