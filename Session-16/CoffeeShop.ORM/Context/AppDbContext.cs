0e0207c724faaf7c3786d48a28048cf810c205a1
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-01-30 21:42:08 +0200

    Add App DB Context and product configuration

--- Session-16/CoffeeShop.ORM/Context/AppDbContext.cs
+++ Session-16/CoffeeShop.ORM/Context/AppDbContext.cs
@@ -1,22 +1,30 @@
 ﻿using CoffeeShop.Models;
+using CoffeeShop.ORM.Configurations;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Reflection.Emit;
 
-namespace CoffeeShop.ORM {
+namespace CoffeeShop.ORM.Context
+{
 
-    public class AppDbContext : DbContext {
+    public class AppDbContext : DbContext
+    {
         public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+
             base.OnModelCreating(modelBuilder);
+
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 
-            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoffeeshopDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CoffeeShopDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
 
             base.OnConfiguring(optionsBuilder);
+
         }
 
     }
