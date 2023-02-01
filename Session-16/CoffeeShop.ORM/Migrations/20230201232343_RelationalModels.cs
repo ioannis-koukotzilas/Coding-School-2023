bcc16260ababcc90d327deb8a6b9bd09cbf115ee
ioannis koukotzilas <ioannis@monoscopic.net>
2023-02-02 01:25:49 +0200

    Remove comments from DbContext

---
+++ Session-16/CoffeeShop.ORM/Migrations/20230201232343_RelationalModels.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class RelationalModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductCategoryID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier")
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProductType = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TransactionLine",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<int>(type: "int", nullable: false),
+                    TotalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TransactionLine", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_TransactionLine_Products_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Products",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ProductCategoryID",
+                table: "Products",
+                column: "ProductCategoryID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TransactionLine_ProductID",
+                table: "TransactionLine",
+                column: "ProductID",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryID",
+                table: "Products",
+                column: "ProductCategoryID",
+                principalTable: "ProductCategories",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_ProductCategories_ProductCategoryID",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropTable(
+                name: "TransactionLine");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ProductCategoryID",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ProductCategoryID",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ID",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
