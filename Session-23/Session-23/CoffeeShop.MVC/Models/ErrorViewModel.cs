9b9dc2cdb727875623c109969644effccf6e24fe
Ioannis Koukotzilas <73739698+ioannis-koukotzilas@users.noreply.github.com>
2023-02-11 01:40:44 +0200

    Delete Session-23/Session-23 directory

--- Session-23/Session-23/CoffeeShop.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CoffeeShop.MVC.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
