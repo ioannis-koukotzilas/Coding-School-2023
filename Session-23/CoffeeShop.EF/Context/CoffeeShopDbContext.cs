047107415bb293eaa1d610beb957326f3bb9442c
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-11 21:11:23 +0200

    Apply initial migration. Check migration for errors. Update Db. Run SQL queries on Azure Data Studio. Seems that everything working as expected.

--- Session-23/CoffeeShop.EF/Context/CoffeeShopDbContext.cs
+++ Session-23/CoffeeShop.EF/Context/CoffeeShopDbContext.cs
@@ -16,12 +16,27 @@ namespace CoffeeShop.EF.Context {
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductCategoryConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionLineConfiguration());
             base.OnModelCreating(modelBuilder);
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
 
-            optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=CoffeeShop; User ID=sa; Password=uU904291$%!; Integrated Security=False; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer("Data Source=localhost; " +
+                "Initial Catalog=CoffeeShop; " +
+                "User ID=sa; " +
+                "Password=uU904291$%!; " +
+                "Integrated Security=False; " +
+                "Connect Timeout=30; " +
+                "Encrypt=False; " +
+                "TrustServerCertificate=False; " +
+                "ApplicationIntent=ReadWrite; " +
+                "MultiSubnetFailover=False");
+
             base.OnConfiguring(optionsBuilder);
 
         }
