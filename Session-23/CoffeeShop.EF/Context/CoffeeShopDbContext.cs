b1fd14d2a1a3d3932d4fa5ec5426c5260f7b2f2c
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-11 13:06:26 +0200

    SQL Server isn’t supported on the ARM architecture. Install Docker and the container image for Azure SQL Edge. Azure SQL Edge is a relational database engine built on the latest versions of the SQL Server. Add Db Context and the connection string.

---
+++ Session-23/CoffeeShop.EF/Context/CoffeeShopDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.EF.Context {
+
+    public class CoffeeShopDbContext : DbContext {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionLine> TransactionLines { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            optionsBuilder.UseSqlServer("Data Source=localhost; Initial Catalog=CoffeeShop; User ID=sa; Password=uU904291$%!; Integrated Security=False; Connect Timeout=30; Encrypt=False; TrustServerCertificate=False; ApplicationIntent=ReadWrite; MultiSubnetFailover=False");
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+    }
+
+}
