4b81402d7449234ea12af7c3df3d44cac54c881a
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-11 22:47:18 +0200

    Add EntityRepo Interface. Add CustomerRepo. Build Add, Delete, GetAll, GetById, and Updade methods using CoffeeShop Db context.

---
+++ Session-23/CoffeeShop.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using CoffeeShop.EF.Context;
+using CoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.EF.Repositories {
+
+    public class CustomerRepo : IEntityRepo<Customer> {
+
+        public void Add(Customer entity) {
+            using var context = new CoffeeShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            using var context = new CoffeeShopDbContext();
+            var dbCustomer = context.Customers.Where(c => c.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            context.Remove(dbCustomer);
+            context.SaveChanges();
+        }
+
+        public IList<Customer> GetAll() {
+            using var context = new CoffeeShopDbContext();
+            return context.Customers.Include(c => c.Transactions).ToList();
+        }
+
+        public Customer? GetById(int id) {
+            using var context = new CoffeeShopDbContext();
+            return context.Customers.Where(c => c.Id == id).Include(c => c.Transactions).SingleOrDefault();
+        }
+
+        public void Update(int id, Customer entity) {
+            using var context = new CoffeeShopDbContext();
+            var dbCustomer = context.Customers.Where(c => c.Id == id).SingleOrDefault();
+            if (dbCustomer is null)
+                return;
+            dbCustomer.Code = entity.Code;
+            dbCustomer.Description = entity.Description;
+            dbCustomer.Transactions = entity.Transactions;
+            context.SaveChanges();
+        }
+
+    }
+
+}
---
+++ Session-23/CoffeeShop.EF/Repositories/IEntityRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CoffeeShop.EF.Repositories {
+
+    public interface IEntityRepo<TEntity> {
+
+        IList<TEntity> GetAll();
+
+        TEntity? GetById(int id);
+
+        void Add(TEntity entity);
+
+        void Update(int id, TEntity entity);
+
+        void Delete(int id);
+
+    }
+
+}
+
