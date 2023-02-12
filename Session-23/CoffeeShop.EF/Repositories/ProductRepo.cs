4cdb4a789ed2de960d0ace4391ebfffb61da1ba2
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-12 20:50:28 +0200

    Add product repo

---
+++ Session-23/CoffeeShop.EF/Repositories/ProductRepo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using CoffeeShop.EF.Context;
+using CoffeeShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeShop.EF.Repositories {
+
+    public class ProductRepo : IEntityRepo<Product> {
+
+        public IList<Product> GetAll() {
+            using var context = new CoffeeShopDbContext();
+            return context.Products.Include(p => p.TransactionLines).ToList();
+        }
+
+        public Product? GetById(int id) {
+            using var context = new CoffeeShopDbContext();
+            return context.Products.Where(p => p.Id == id).Include(p => p.TransactionLines).SingleOrDefault();
+        }
+
+        public void Add(Product entity) {
+            using var context = new CoffeeShopDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Update(int id, Product entity) {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(p => p.Id == id).SingleOrDefault();
+            if (dbProduct is null)
+                return;
+            dbProduct.Code = entity.Code;
+            dbProduct.Description = entity.Description;
+            dbProduct.Price = entity.Price;
+            dbProduct.Cost = entity.Cost;
+            dbProduct.TransactionLines = entity.TransactionLines;
+            context.SaveChanges();
+        }
+
+        public void Delete(int id) {
+            using var context = new CoffeeShopDbContext();
+            var dbProduct = context.Products.Where(p => p.Id == id).SingleOrDefault();
+            if (dbProduct is null)
+                return;
+            context.Remove(dbProduct);
+            context.SaveChanges();
+        }
+
+    }
+
+}
