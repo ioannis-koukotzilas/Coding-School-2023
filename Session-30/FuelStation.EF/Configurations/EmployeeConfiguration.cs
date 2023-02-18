3a8b0aac1f4fe15262b38eabed96685e326a494c
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-18 22:28:50 +0200

    EF initial build

---
+++ Session-30/FuelStation.EF/Configurations/EmployeeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using FuelStation.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FuelStation.EF.Configurations {
+
+    public class EmployeeConfiguration : IEntityTypeConfiguration<Employee> {
+
+        public void Configure(EntityTypeBuilder<Employee> builder) {
+
+            
+
+        }
+
+    }
+
+}
---
+++ Session-30/FuelStation.EF/Context/FuelStationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FuelStation.EF.Configurations;
+using FuelStation.Models;
+
+namespace FuelStation.EF.Context {
+
+    public class FuelStationDbContext : DbContext {
+
+        public DbSet<Customer> Customers { get; set; } = null!;
+        public DbSet<Employee> Employees { get; set; } = null!;
+        public DbSet<Item> Items { get; set; } = null!;
+        public DbSet<Transaction> Transactions { get; set; } = null!;
+        public DbSet<TransactionLine> TransactionLines { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            var connectionString = "Data Source = localhost; Initial Catalog = FuelStation; User Id = sa; Password = uU904291$%!";
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+    }
+
+}
