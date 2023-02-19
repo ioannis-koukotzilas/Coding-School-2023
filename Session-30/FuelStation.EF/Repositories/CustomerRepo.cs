388bc642985822d5f0ac7ce50f66083d347cb0aa
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-19 12:01:30 +0200

    Add customer repo

--- Session-30/FuelStation.EF/Repositories/CustomerRepo.cs
+++ Session-30/FuelStation.EF/Repositories/CustomerRepo.cs
@@ -1,27 +1,59 @@
 ﻿using FuelStation.Models;
+using FuelStation.EF.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace FuelStation.EF.Repositories {
 
     public class CustomerRepo : IEntityRepo<Customer> {
 
         public IList<Customer> GetAll() {
-            throw new NotImplementedException();
+            using var dbContext = new FuelStationDbContext();
+            var dbCustomer = dbContext.Customers.Include(c => c.Transactions).ToList();
+            return dbCustomer;   
         }
 
         public Customer? GetById(int id) {
-            throw new NotImplementedException();
+            using var dbContext = new FuelStationDbContext();
+            var dbCustomer = dbContext.Customers.Where(c => c.Id == id).Include(c => c.Transactions).SingleOrDefault();
+            return dbCustomer;
         }
 
         public void Add(Customer entity) {
-            throw new NotImplementedException();
+            using var dbContext = new FuelStationDbContext();
+
+            if (entity.Id != 0) {
+                throw new ArgumentException("Given entity should not have an ID set", nameof(entity));
+            }
+
+            dbContext.Add(entity);
+            dbContext.SaveChanges();   
         }
 
         public void Update(int id, Customer entity) {
-            throw new NotImplementedException();
+            using var dbContext = new FuelStationDbContext();
+            var dbCustomer = dbContext.Customers.Where(c => c.Id == id).SingleOrDefault();
+
+            if (dbCustomer == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbCustomer.Name = entity.Name;
+            dbCustomer.Surname = entity.Surname;
+            dbCustomer.CardNumber = entity.CardNumber;
+
+            dbContext.SaveChanges();
         }
 
         public void Delete(int id) {
-            throw new NotImplementedException();
+            using var dbContext = new FuelStationDbContext();
+            var dbCustomer = dbContext.Customers.Where(c => c.Id == id).SingleOrDefault();
+
+            if (dbCustomer == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbContext.Remove(dbCustomer);
+            dbContext.SaveChanges();
         }
 
     }
