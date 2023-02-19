77999b4452124f79e4227b5661341dc77ad63b33
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-19 02:11:03 +0200

    Repos initial build

---
+++ Session-30/FuelStation.EF/Repositories/CustomerRepo.cs
@@ -0,0 +1,30 @@
+﻿using FuelStation.Models;
+
+namespace FuelStation.EF.Repositories {
+
+    public class CustomerRepo : IEntityRepo<Customer> {
+
+        public IList<Customer> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public Customer? GetById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Customer entity) {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, Customer entity) {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id) {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
+
---
+++ Session-30/FuelStation.EF/Repositories/IEntityRepo.cs
@@ -0,0 +1,13 @@
+﻿namespace FuelStation.EF.Repositories {
+
+    public interface IEntityRepo<TEntity> {
+
+        IList<TEntity> GetAll();
+        TEntity? GetById(int id);
+        void Add(TEntity entity);
+        void Update(int id, TEntity entity);
+        void Delete(int id);
+
+    }
+
+}
