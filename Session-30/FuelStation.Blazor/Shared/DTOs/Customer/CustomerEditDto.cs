cd978b0af1a65ef0b6d89961f148f3bd12fb80b1
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-19 23:27:13 +0200

    Add validations to customer edit

--- Session-30/FuelStation.Blazor/Shared/DTOs/Customer/CustomerEditDto.cs
+++ Session-30/FuelStation.Blazor/Shared/DTOs/Customer/CustomerEditDto.cs
@@ -1,10 +1,15 @@
-﻿namespace FuelStation.Blazor.Shared.DTOs.Customer {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FuelStation.Blazor.Shared.DTOs.Customer {
 
     public class CustomerEditDto {
 
         public int Id { get; set; }
+        [Required(ErrorMessage = "Customer's name is required field")]
         public string Name { get; set; } = null!;
+        [Required(ErrorMessage = "Customer's surname is required field")]
         public string Surname { get; set; } = null!;
+        [Required(ErrorMessage = "Required field")]
         public string CardNumber { get; set; } = null!;
 
     }
