3301515dddd28293f2cb3255c71bf0652777912d
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-19 23:00:01 +0200

    Add put task to C controller. Code refactoring

--- Session-30/FuelStation.EF/Repositories/IEntityRepo.cs
+++ Session-30/FuelStation.EF/Repositories/IEntityRepo.cs
@@ -5,7 +5,7 @@
         Task<IList<TEntity>> GetAllAsync();
         Task<TEntity?> GetByIdAsync(int id);
         Task AddAsync(TEntity entity);
-        void Update(int id, TEntity entity);
+        Task UpdateAsync(int id, TEntity entity);
         Task DeleteAsync(int id);
 
     }
