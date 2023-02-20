4445b05c079240c18c042b91279c670ee608235a
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-20 04:54:51 +0200

    Add transaction line repo

---
+++ Session-30/FuelStation.EF/Repositories/EmployeeRepo.cs
@@ -0,0 +1,74 @@
+﻿using FuelStation.Models;
+using FuelStation.EF.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuelStation.EF.Repositories {
+
+    public class EmployeeRepo : IEntityRepo<Employee> {
+
+        public async Task<IList<Employee>> GetAllAsync() {
+            using var dbContext = new FuelStationDbContext();
+            var dbEmployee = await dbContext.Employees
+                .Include(e => e.Transactions)
+                .ToListAsync();
+            return dbEmployee;
+        }
+
+        public async Task<Employee?> GetByIdAsync(int id) {
+            using var dbContext = new FuelStationDbContext();
+            var dbEmployee = await dbContext.Employees
+                .Where(e => e.Id == id)
+                .Include(e => e.Transactions)
+                .SingleOrDefaultAsync();
+            return dbEmployee;
+        }
+
+        public async Task AddAsync(Employee entity) {
+            using var dbContext = new FuelStationDbContext();
+
+            if (entity.Id != 0) {
+                throw new ArgumentException("Given entity should not have an ID set", nameof(entity));
+            }
+
+            await dbContext.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, Employee entity) {
+            using var dbContext = new FuelStationDbContext();
+            var dbEmployee = await dbContext.Employees
+                .Where(e => e.Id == id)
+                .Include(e => e.Transactions)
+                .SingleOrDefaultAsync();
+
+            if (dbEmployee == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbEmployee.Name = entity.Name;
+            dbEmployee.Surname = entity.Surname;
+            dbEmployee.HireDateStart = entity.HireDateStart;
+            dbEmployee.HireDateEnd = entity.HireDateEnd;
+            dbEmployee.SallaryPerMonth = entity.SallaryPerMonth;
+            dbEmployee.EmployeeType = entity.EmployeeType;
+           
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id) {
+            using var dbContext = new FuelStationDbContext();
+            var dbEmployee = await dbContext.Employees
+                .Where(e => e.Id == id)
+                .SingleOrDefaultAsync();
+
+            if (dbEmployee == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbContext.Remove(dbEmployee);
+            await dbContext.SaveChangesAsync();
+        }
+
+    }
+
+}
---
+++ Session-30/FuelStation.EF/Repositories/TransactionLineRepo.cs
@@ -0,0 +1,79 @@
+﻿using FuelStation.Models;
+using FuelStation.EF.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace FuelStation.EF.Repositories {
+
+    public class TranactionLineRepo : IEntityRepo<TransactionLine> {
+
+        public async Task<IList<TransactionLine>> GetAllAsync() {
+            using var dbContext = new FuelStationDbContext();
+            var dbTransactionLine = await dbContext.TransactionLines
+                .Include(tl => tl.Item)
+                .Include(tl => tl.Transaction)
+                .ToListAsync();
+            return dbTransactionLine;
+        }
+
+        public async Task<TransactionLine?> GetByIdAsync(int id) {
+            using var dbContext = new FuelStationDbContext();
+            var dbTransactionLine = await dbContext.TransactionLines
+                .Where(tl => tl.Id == id)
+                .Include(tl => tl.Item)
+                .Include(tl => tl.Transaction)
+                .SingleOrDefaultAsync();
+            return dbTransactionLine;
+        }
+
+        public async Task AddAsync(TransactionLine entity) {
+            using var dbContext = new FuelStationDbContext();
+
+            if (entity.Id != 0) {
+                throw new ArgumentException("Given entity should not have an ID set", nameof(entity));
+            }
+
+            await dbContext.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(int id, TransactionLine entity) {
+            using var dbContext = new FuelStationDbContext();
+            var dbTransactionLine = await dbContext.TransactionLines
+                .Where(tl => tl.Id == id)
+                .Include(tl => tl.Item)
+                .Include(tl => tl.Transaction)
+                .SingleOrDefaultAsync();
+
+            if (dbTransactionLine == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbTransactionLine.Quantity = entity.Quantity;
+            dbTransactionLine.ItemPrice = entity.ItemPrice;
+            dbTransactionLine.NetValue = entity.NetValue;
+            dbTransactionLine.DiscountPercent = entity.DiscountPercent;
+            dbTransactionLine.DiscountValue = entity.DiscountValue;
+            dbTransactionLine.TotalValue = entity.TotalValue;
+            dbTransactionLine.TransactionId = entity.TransactionId;
+            dbTransactionLine.ItemId = entity.ItemId;
+           
+            await dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id) {
+            using var dbContext = new FuelStationDbContext();
+            var dbTransaction = await dbContext.Transactions.
+                Where(t => t.Id == id).
+                SingleOrDefaultAsync();
+
+            if (dbTransaction == null) {
+                throw new KeyNotFoundException($"Given ID '{id}' was not found in the database");
+            }
+
+            dbContext.Remove(dbTransaction);
+            await dbContext.SaveChangesAsync();
+        }
+
+    }
+
+}
