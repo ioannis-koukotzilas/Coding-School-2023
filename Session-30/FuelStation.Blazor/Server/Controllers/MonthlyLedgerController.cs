36fb1e0e5f771cacc3e9b40344118c4e3aea4548
Ioannis Koukotzilas <ioannis@monoscopic.net>
2023-02-27 02:49:49 +0200

    Added authentication logic to the client-side. Implemented role-based access control for application features. Updated the user interface to display error messages when access is denied. Implemented ledger report.

--- Session-30/FuelStation.Blazor/Server/Controllers/MonthlyLedgerController.cs
+++ Session-30/FuelStation.Blazor/Server/Controllers/MonthlyLedgerController.cs
@@ -1,9 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Globalization;
+using FuelStation.Blazor.Shared.DTOs.Reports;
+using FuelStation.EF.Repositories;
+using FuelStation.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FuelStation.Web.Blazor.Server.Controllers {
+
     [Route("[controller]")]
     [ApiController]
     public class MonthlyLedgerController : ControllerBase {
 
+        private readonly IEntityRepo<Employee> _employeeRepo;
+        private readonly IEntityRepo<Transaction> _transactionRepo;
+
+        public MonthlyLedgerController(IEntityRepo<Employee> employeeRepo, IEntityRepo<Transaction> transactionRepo) {
+            _employeeRepo = employeeRepo;
+            _transactionRepo = transactionRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<MonthlyLedgerDto>> Get() {
+
+            List<MonthlyLedgerDto> monthlyLedgers = new();
+
+            var dbTransactions = await _transactionRepo.GetAllAsync();
+
+            var dbEmployees = await _employeeRepo.GetAllAsync();
+
+            decimal totalTransactions = 0;
+            decimal totalSallaries = 0;
+            decimal rent = 5000m;
+
+            foreach (var t in dbTransactions) {
+                totalTransactions += t.TotalValue;
+            }
+
+            foreach (var e in dbEmployees) {
+                totalSallaries += e.SallaryPerMonth;
+            }
+
+            var groupTransactions = dbTransactions
+
+                .GroupBy(transactions => new {
+                    transactions.Date.Year,
+                    transactions.Date.Month
+                })
+
+                .Select(group => new MonthlyLedgerDto {
+                    Year = group.Key.Year,
+                    Month = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(group.Key.Month),
+                    Income = totalTransactions,
+                    Expenses = totalSallaries + rent,
+                    Total = totalTransactions - (totalSallaries + rent)
+                });
+
+            foreach (var grouped in groupTransactions) {
+                monthlyLedgers.Add(grouped);
+            }
+
+            return groupTransactions;
+        }
+
     }
+
 }
--- Session-30/FuelStation.Blazor/Shared/DTOs/Report/MonthlyLedgerDto.cs
+++ Session-30/FuelStation.Blazor/Shared/DTOs/Report/MonthlyLedgerDto.cs
@@ -3,7 +3,7 @@
     public class MonthlyLedgerDto {
 
         public int Year { get; set; }
-        public string Month { get; set; } = null!;
+        public string? Month { get; set; }
         public decimal Income { get; set; }
         public decimal Expenses { get; set; }
         public decimal Total { get; set; }
